31ac4380ef1b44276fc78eec48b4505911c54349
mohamdkhalid <mohamdkhaled1512@gmail.com>
2024-12-15 04:56:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMenu(){
+    const nav =document.getElementById("nav-links");
+    nav.classList.toggle("active");
+}
+
+
+function validateForm() {
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+
+      if (name === "") {
+        alert("Please enter your name.");
+        return false; 
+      }
+      if (email === "") {
+        alert("Please enter a email.");
+        return false;
+      }
+      if (phone === "") {
+        alert("Please enter your phone.");
+        return false;
+      }
+    alert("Form submitted successfully!");
+    return true;
+  }
